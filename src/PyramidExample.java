25def8f4b0987fef6ebe05639c0c5e2086e11c70
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-01 14:46:06 -0400

    second commit

--- src/PyramidExample.java
+++ src/PyramidExample.java
@@ -1,7 +1,8 @@
 public class PyramidExample {
     public static void main(String[] args){
         for(int i = 1; i<=5; j++){
-
+            System.out.println("*");
         }
+        System.out.println();
     }
 }
